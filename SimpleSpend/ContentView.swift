06d8f7c7584fbdd5db80ee43d080ca37989ee691
omarrris <omarr.sheikh@gmail.com>
2024-06-24 19:32:15 -0400

    Initial Commit

---
+++ SimpleSpend/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SimpleSpend
+//
+//  Created by Omar Sheikh on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleSpend/SimpleSpendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSpendApp.swift
+//  SimpleSpend
+//
+//  Created by Omar Sheikh on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSpendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
