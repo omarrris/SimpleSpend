5b7518e9c7cb114e64d2cf7dbc5911ae91e782b0
omarrris <omarr.sheikh@gmail.com>
2024-06-24 22:19:28 -0400

    settings view

---
+++ SimpleSpend/Screens/Settings.swift
@@ -0,0 +1,47 @@
+
+import SwiftUI
+import RealmSwift
+
+struct Settings: View {
+    @State private var showEraseConfirmation = false
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink {
+                    Categories()
+                } label: {
+                    HStack {
+                        Text("Categories")
+                    }
+                }
+                
+                Button(role: .destructive) {
+                    showEraseConfirmation = true
+                } label: {
+                    Text("Erase Data")
+                }
+                .alert(isPresented: $showEraseConfirmation) {
+                    Alert(
+                        title: Text("Are you sure?"),
+                        message: Text("This action cannot be undone."),
+                        primaryButton: .destructive(Text("Erase data")) {
+                            let realm = try! Realm()
+                            realm.beginWrite()
+                            realm.deleteAll()
+                            try! realm.commitWrite()
+                        },
+                        secondaryButton: .cancel()
+                    )
+                }
+            }
+            .navigationTitle("Settings")
+            .padding(.top, 16)
+        }
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
