f71487683cbcb621ea3f3ed7c87f4dfde1b6ddc0
omarrris <omarr.sheikh@gmail.com>
2024-06-24 22:31:47 -0400

    content view

--- SimpleSpend/ContentView.swift
+++ SimpleSpend/ContentView.swift
@@ -1,23 +1,38 @@
-//
-//  ContentView.swift
-//  SimpleSpend
-//
-//  Created by Omar Sheikh on 6/24/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var realmManager = RealmManager()
+    @State private var isSignedIn = UserDefaults.standard.string(forKey: "userIdentifier") != nil
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TabView {
+                Expenses(expenses: realmManager.expenses)
+                    .environmentObject(realmManager)
+                    .tabItem {
+                        Label("Expenses", systemImage: "tray.and.arrow.up.fill")
+                    }
+                
+                Reports()
+                    .environmentObject(realmManager)
+                    .tabItem {
+                        Label("Reports", systemImage: "chart.bar.fill")
+                    }
+                
+                Add()
+                    .environmentObject(realmManager)
+                    .tabItem {
+                        Label("Add", systemImage: "plus")
+                    }
+                
+                Settings()
+                    .environmentObject(realmManager)
+                    .tabItem {
+                        Label("Settings", systemImage: "gearshape.fill")
+                    }
+            }
         }
-        .padding()
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- SimpleSpend/SimpleSpendApp.swift
+++ SimpleSpend/SimpleSpendApp.swift
@@ -1,17 +1,36 @@
-//
-//  SimpleSpendApp.swift
-//  SimpleSpend
-//
-//  Created by Omar Sheikh on 6/24/24.
-//
 
+import Sentry
 import SwiftUI
 
 @main
-struct SimpleSpendApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+struct SimpleSpendApp: SwiftUI.App {
+   init() {
+       SentrySDK.start { options in
+           if let dsn = Bundle.main.infoDictionary?["SENTRY_DSN"] as? String {
+               let fullDsn = "https://\(dsn)"
+               options.dsn = fullDsn
+           }
+           if let env = Bundle.main.infoDictionary?["SENTRY_ENVIRONMENT"] as? String {
+               let debug = env.compare("development") == .orderedSame
+               options.debug = debug
+               options.environment = env
+           }
+           if let tracesSampleRate = Bundle.main.infoDictionary?["SENTRY_TRACES_SAMPLE_RATE"] as? String {
+               options.tracesSampleRate = NSNumber(value: Double(tracesSampleRate)!)
+           }
+           if let profilesSampleRate = Bundle.main.infoDictionary?["SENTRY_PROFILES_SAMPLE_RATE"] as? String {
+               options.profilesSampleRate = NSNumber(value: Double(profilesSampleRate)!)
+           }
+           
+           options.enableAppHangTracking = true
+           options.enableUserInteractionTracing = true
+           options.attachViewHierarchy = true
+       }
+   }
+   
+   var body: some Scene {
+       WindowGroup {
+           ContentView()
+       }
+   }
 }
