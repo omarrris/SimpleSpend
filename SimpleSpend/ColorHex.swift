dccf5f6934dc29d5ad7c10de41a8c7183ae95826
omarrris <omarr.sheikh@gmail.com>
2024-07-05 15:57:12 -0400

    final commit

---
+++ SimpleSpend/ColorHex.swift
@@ -0,0 +1,8 @@
+//
+//  ColorHex.swift
+//  SimpleSpend
+//
+//  Created by Omar Sheikh on 7/5/24.
+//
+
+import Foundation
--- SimpleSpend/Screens/SignIn.swift
+++ SimpleSpend/Screens/SignIn.swift
@@ -1,55 +1,36 @@
 import SwiftUI
-import AuthenticationServices
 
 struct SignIn: View {
-    @Binding var isSignedIn: Bool
-
     var body: some View {
-        VStack {
-            Image("simplespendmain")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 300, height: 200)
-                .padding(.top, -50)
-            
-            SignInWithAppleButton(.signIn) { request in
-                request.requestedScopes = [.fullName, .email]
-            } onCompletion: { result in
-                switch result {
-                case .success(let authorization):
-                    handleAuthorization(authorization: authorization)
-                case .failure(let error):
-                    print("Authorization failed: \(error.localizedDescription)")
+        NavigationView {
+            VStack {
+                Image("simplespendmain")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 200)
+                    .padding(.top, -50)
+                
+                NavigationLink(destination: ContentView()) {
+                    Text("Get Started")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .background(
+                            LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: .leading, endPoint: .trailing)
+                        )
+                        .cornerRadius(25)
                 }
+                .padding()
             }
-            .signInWithAppleButtonStyle(.black)
-            .frame(width: 200, height: 45)
-            .padding()
-        }
-        .darkMode()
-    }
-    private func handleAuthorization(authorization: ASAuthorization) {
-        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
-            let userIdentifier = appleIDCredential.user
-            let fullName = appleIDCredential.fullName
-            let email = appleIDCredential.email
-            
-            print("User ID: \(userIdentifier)")
-            print("User Full Name: \(String(describing: fullName))")
-            print("User Email: \(String(describing: email))")
-            
-            UserDefaults.standard.set(userIdentifier, forKey: "userIdentifier")
-                       
-                       // Update the sign-in state
-                       isSignedIn = true
+            .darkMode()
         }
     }
 }
 
 struct SignInView_Previews: PreviewProvider {
     static var previews: some View {
-        SignIn(isSignedIn: .constant(false))
+        SignIn()
             .darkMode()
     }
 }
-
